40b4228622ff4da9e52a7db3fa370509eac062a6
CK <73918057+yeock@users.noreply.github.com>
2021-07-30 22:51:57 +0800

    Deb package build automation

--- automation.sh
+++ automation.sh
@@ -2,16 +2,14 @@
 
 bzr builddeb -- -us -uc > dependencies.txt
 sed -i -n '/Unmet build dependencies: /p' dependencies.txt
-sed -i 's/dpkg-checkbuilddeps: error: Unmet build dependencies: //g' dependencies.txt
-sed -i 's/(\([>=]*\) \([0-9.]*\))//g' dependencies.txt
-sed -i 's/|//g' dependencies.txt
+sed -i 's/dpkg-checkbuilddeps: error: Unmet build dependencies: //g; s/(\([>=]*\) \([0-9.]*\))//g; s/|//g' dependencies.txt
 
 if [ -s dependencies.txt ]
 then
 	apt install $(cat dependencies.txt)
 	bzr builddeb -- -us -uc
 else
-	echo "NO MISSING DEPENDENCIES..."
+	echo "PACKAGE BUILT!\nNO MISSING DEPENDENCIES..."
 fi
 
 
