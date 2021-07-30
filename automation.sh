f4a5d514344d7eb6797922aa4800a26c19507230
CK <73918057+yeock@users.noreply.github.com>
2021-07-30 22:55:02 +0800

    deb package build automation

--- automation.sh
+++ automation.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-bzr builddeb -- -us -uc > dependencies.txt
+bzr builddeb -- -us -uc > dependencies.txt # or dpkg-buildpackage -us -uc etc...
 sed -i -n '/Unmet build dependencies: /p' dependencies.txt
 sed -i 's/dpkg-checkbuilddeps: error: Unmet build dependencies: //g; s/(\([>=]*\) \([0-9.]*\))//g; s/|//g' dependencies.txt
 
@@ -12,4 +12,4 @@ else
 	echo "PACKAGE BUILT!\nNO MISSING DEPENDENCIES..."
 fi
 
-
+# auto remove dependencies.txt by adding rm command...
